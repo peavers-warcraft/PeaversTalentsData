0ad1cb88231ad636fb03cd462141e48d0c97c272
Chris Turner <peavers@gmail.com>
2025-02-27 16:44:26 -0600

    Database update: update-communitymythicdb

--- src/Data/CommunityMythicDB.lua
+++ src/Data/CommunityMythicDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.CommunityMythicDB = addonTable.CommunityMythicDB or {}
 
 local talentData = {
-	updated = "2025-02-27 03:37:36",
+	updated = "2025-02-27 22:24:30",
 
 	[7] = {
 		specs = {
