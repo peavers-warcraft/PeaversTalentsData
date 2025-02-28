ccf992c2b7971ef9b689f479efe649f549db8a2f
Chris Turner <peavers@gmail.com>
2025-02-28 14:19:39 -0600

    Database update: update-communitymiscdb

--- src/Data/CommunityMiscDB.lua
+++ src/Data/CommunityMiscDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.CommunityMiscDB = addonTable.CommunityMiscDB or {}
 
 local talentData = {
-	updated = "2025-02-27 22:24:30",
+	updated = "2025-02-28 19:52:22",
 
 	[2] = {
 		specs = {
