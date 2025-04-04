c895dacc41ee3f9b4cf3b83f01b423244ba2c12d
Chris Turner <peavers@gmail.com>
2025-04-04 08:27:06 -0700

    Database update: update-top-playersheroicraiddb

--- src/Data/TopPlayersHeroicRaidDB.lua
+++ src/Data/TopPlayersHeroicRaidDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.TopPlayersHeroicRaidDB = addonTable.TopPlayersHeroicRaidDB or {}
 
 local talentData = {
-	updated = "2025-04-04 15:08:42",
+	updated = "2025-04-04 15:25:36",
 
 	[1] = {
 		specs = {
