045ebdbca94b1d1bf614befb2fd7836a53cec324
Chris Turner <peavers@gmail.com>
2026-04-09 23:00:49 -0500

    Database update: most-popular talent builds most-popular-update-2026-04-10-040047

--- src/Data/MostPopularMiscDB.lua
+++ src/Data/MostPopularMiscDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.MostPopularMiscDB = addonTable.MostPopularMiscDB or {}
 
 local talentData = {
-	updated = "2026-04-09 04:00:46",
+	updated = "2026-04-10 04:00:47",
 
 	[1] = {
 		specs = {
