4024345814007441a274fb574fc6627e9ae0f797
Chris Turner <peavers@gmail.com>
2026-05-14 23:00:52 -0500

    Database update: Wowhead talent builds wowhead-update-2026-05-15-040049

--- src/Data/WowheadMiscDB.lua
+++ src/Data/WowheadMiscDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.WowheadMiscDB = addonTable.WowheadMiscDB or {}
 
 local talentData = {
-	updated = "2026-05-14 04:00:47",
+	updated = "2026-05-15 04:00:49",
 
 	[1] = {
 		specs = {
