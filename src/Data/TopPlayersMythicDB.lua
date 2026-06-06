0b71a3a1b7e9ee3aaa2ed3e73958b6f71702f4e1
Chris Turner <peavers@gmail.com>
2026-06-06 08:43:43 -0500

    Database update: top-players-update-top-playersmythicdb-2026-06-06-134341

--- src/Data/TopPlayersMythicDB.lua
+++ src/Data/TopPlayersMythicDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.TopPlayersMythicDB = addonTable.TopPlayersMythicDB or {}
 
 local talentData = {
-	updated = "2026-06-06 05:42:10",
+	updated = "2026-06-06 13:43:40",
 
 	[1] = {
 		specs = {
