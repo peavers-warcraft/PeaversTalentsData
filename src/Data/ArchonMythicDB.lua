b606fdada511bfad30d7ab30e048bfc4a8895a16
Chris Turner <peavers@gmail.com>
2026-06-11 16:42:11 -0500

    Database update: archon-update-archonmythicdb-2026-06-11-214209

--- src/Data/ArchonMythicDB.lua
+++ src/Data/ArchonMythicDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.ArchonMythicDB = addonTable.ArchonMythicDB or {}
 
 local talentData = {
-	updated = "2026-06-11 13:42:26",
+	updated = "2026-06-11 21:42:09",
 
 	[1] = {
 		specs = {
